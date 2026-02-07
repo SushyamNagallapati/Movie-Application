9e9bfbe4d6ce7e93c0ca99a947760042ac83d7a7
Sushyam Nagallapati <sushyamraju@gmail.com>
2026-02-07 18:20:06 -0500

    first commit

---
+++ src/services/Api.js
@@ -0,0 +1,18 @@
+const API_KEY = "6117c14359ca063a1f1705dc599d85ac";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query,
+    )}`,
+  );
+  const data = await response.json();
+  return data.results;
+};
